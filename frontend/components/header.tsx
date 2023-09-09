9fb45d1a5ada7deef37a346eddaa8da9db76712b
NoobBaez <haniel_0101@hotmail.com>
2023-09-09 14:59:43 -0400

    Add Header

---
+++ frontend/components/header.tsx
@@ -0,0 +1,32 @@
+import { rem, Header, Group, Highlight, Stack } from "@mantine/core";
+import { ColorToggle } from "./color-toggle";
+
+const HEADER_HEIGHT = rem(60);
+export const TITLE = "Github Commit History";
+const WRONG_TITLE = "Jithub Commit History";
+
+export function HeaderCustom() {
+  return (
+    <Header height={HEADER_HEIGHT} sx={{ borderBottom: 0 }} mb={120}>
+      <Group position="apart" p="md">
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Highlight highlightColor="red" m={0} highlight={WRONG_TITLE}>
+            {`- ${WRONG_TITLE}`}
+          </Highlight>
+          <Highlight highlightColor="lime" m={0} highlight={TITLE}>
+            {`+ ${TITLE}`}
+          </Highlight>
+        </div>
+        <Stack justify="center" align="baseline"></Stack>
+
+        <ColorToggle />
+      </Group>
+    </Header>
+  );
+}
