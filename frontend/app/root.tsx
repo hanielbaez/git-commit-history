47bc39c8447957d5b647e67c28238ca024218926
NoobBaez <haniel_0101@hotmail.com>
2023-09-09 14:56:45 -0400

    Add ColorToggle component

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -6,27 +6,46 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import { MantineProvider, createEmotionCache } from "@mantine/core";
+import {
+  type ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+  createEmotionCache,
+} from "@mantine/core";
 import { StylesPlaceholder } from "@mantine/remix";
+import { useState } from "react";
 
 createEmotionCache({ key: "mantine" });
 
 export default function App() {
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <html lang="en">
-        <head>
-          <StylesPlaceholder />
-          <Meta />
-          <Links />
-        </head>
-        <body>
-          <Outlet />
-          <ScrollRestoration />
-          <Scripts />
-          <LiveReload />
-        </body>
-      </html>
-    </MantineProvider>
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        theme={{ colorScheme }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        <html lang="en">
+          <head>
+            <StylesPlaceholder />
+            <Meta />
+            <Links />
+          </head>
+          <body>
+            <Outlet />
+            <ScrollRestoration />
+            <Scripts />
+            <LiveReload />
+          </body>
+        </html>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 }
