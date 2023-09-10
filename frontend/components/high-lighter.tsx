6d679348f1859cb9d6e6bfa5e470da7ef7f0adf4
NoobBaez <haniel_0101@hotmail.com>
2023-09-10 15:13:39 -0400

    Implement github style to the code high lighter

--- frontend/components/high-lighter.tsx
+++ frontend/components/high-lighter.tsx
@@ -1,6 +1,10 @@
-import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
-import { docco } from "react-syntax-highlighter/dist/cjs/styles/hljs";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { github } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
 export default function Highlighter({ code }: { code: string }) {
-  return <SyntaxHighlighter style={docco}>{code}</SyntaxHighlighter>;
+  return (
+    <SyntaxHighlighter language="typescript" style={github}>
+      {code}
+    </SyntaxHighlighter>
+  );
 }
