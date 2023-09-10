5719498535695d9ee1e9f22c5a16bf681a847da4
NoobBaez <haniel_0101@hotmail.com>
2023-09-10 15:46:11 -0400

    Add base theme

---
+++ frontend/app/theme.ts
@@ -0,0 +1,21 @@
+import { type MantineThemeOverride } from "@mantine/core";
+
+const theme: MantineThemeOverride = {
+  colorScheme: "light",
+
+  colors: {
+    background: ["#f6f8fa"],
+    backgroundDark: ["#24292e"],
+
+    text: ["#333333"],
+    textDark: ["#ffffff"],
+
+    primary: ["#0366d6"],
+    primaryDark: ["#2188FF"],
+
+    border: ["#e1e4e8"],
+    borderDark: ["#586069"],
+  },
+};
+
+export default theme;
