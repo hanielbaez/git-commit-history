9b5eb1d972e601770fb1b14848a02a27f8b11c0a
NoobBaez <haniel_0101@hotmail.com>
2023-09-09 23:30:27 -0400

    Add ActionIcon and name the query string

--- frontend/components/search-intpu.tsx
+++ frontend/components/search-intpu.tsx
@@ -1,18 +1,24 @@
-import { TextInput, type TextInputProps } from "@mantine/core";
-import { IconSearch } from "@tabler/icons-react";
+import { ActionIcon, Input, type TextInputProps } from "@mantine/core";
+import { IconArrowRight, IconSearch } from "@tabler/icons-react";
 
 export function SearchInput(props: TextInputProps) {
   return (
-    <TextInput
+    <Input
       icon={<IconSearch size="1.1rem" stroke={1.5} />}
       radius="xl"
       size="xl"
       maw={700}
       type="search"
+      name="search"
       placeholder="Search commits from any public repository"
       defaultValue="https://github.com/hanielbaez/git-commit-history"
-      rightSectionWidth={42}
-      {...props}
+      rightSectionWidth={45}
+      m="auto"
+      rightSection={
+        <ActionIcon type="submit">
+          <IconArrowRight />
+        </ActionIcon>
+      }
     />
   );
 }
