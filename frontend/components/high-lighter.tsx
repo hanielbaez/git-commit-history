cf600129b1c6c0f72f5e24b96dc0e71de84c970b
NoobBaez <haniel_0101@hotmail.com>
2023-09-10 15:19:48 -0400

    Switch color style

--- frontend/components/high-lighter.tsx
+++ frontend/components/high-lighter.tsx
@@ -1,9 +1,16 @@
+import { useMantineColorScheme } from "@mantine/core";
 import SyntaxHighlighter from "react-syntax-highlighter";
-import { github } from "react-syntax-highlighter/dist/cjs/styles/hljs";
+import {
+  github,
+  nightOwl,
+} from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
 export default function Highlighter({ code }: { code: string }) {
+  const { colorScheme } = useMantineColorScheme();
+  const dark = colorScheme === "dark";
+
   return (
-    <SyntaxHighlighter language="typescript" style={github}>
+    <SyntaxHighlighter language="typescript" style={dark ? nightOwl : github}>
       {code}
     </SyntaxHighlighter>
   );
